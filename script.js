502523cae2ed774b7ce970f25cacc21cb6708807
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-10-03 10:39:04 -0400

    added basic css and javascript vars

---
+++ script.js
@@ -0,0 +1,6 @@
+var timeEl = document.getElementById("time");
+var welcomeEl = document.getElementById("welcome");
+var questionsEl = document.getElementById("questions");
+var initalEl = document.getElementById("inital");
+var startEl = document.getElementById("start");
+
