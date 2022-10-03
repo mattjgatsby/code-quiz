0613f38807d800581c45655ad1f2e9fe8b778828
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-10-03 11:00:40 -0400

    added var questions to js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ var questionsEl = document.getElementById("questions");
 var initalEl = document.getElementById("inital");
 var startEl = document.getElementById("start");
 
+var questions = [
+    {
+        title:"Question", 
+        choices: ["","","",""],
+        answer: ""
+    }
+]
+// after event listner we hide the welcome, unhide the view questions, and start the timeEl.
+function startQuiz () {
+        welcomeEl.setAttribute("class","view");
+        questionsEl.removeAttribute("class");
+
+}
+
+startEl.addEventListener("click", startQuiz);
