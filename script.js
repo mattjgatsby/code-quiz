033cf077842d1cf0543e4dd8dd114835cd800338
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-11-16 10:20:23 -0500

    reworking js functions to include event listeners

--- script.js
+++ script.js
@@ -4,83 +4,50 @@ var questionsEl = document.getElementById("questions");
 var initalEl = document.getElementById("inital");
 var startEl = document.getElementById("start");
 var titleEl = document.getElementById("title");
-var choicesEl = document.getElementById('choices');
+var optionEl = document.querySelectorAll(".option");
 
+var currentQ = 0;
+var currentScore = 0;
+var timeRemaining = 0;
+
+var endingTime;
+var endingScore;
 
 var questions = [
-    {
-        title:"Which is a Boolean value?", 
-        choices: ["True","Nope","Yeah","Nah"],
-        answer: "True",
-    },
-    {
-        title: "Which is NOT a semantic element?",
-        choices: ["Form", "nav", "aside", "div"],
-        answer: "div",
-    },
-    {
-        title: "Which is a self closeing tag",
-        choices: ["<aside>", "<figure>", "<article>", "<br>"],
-        answer: "<br>",
-    },
-    {
-        title: "Which comparison operator means strictly equal to?",
-        choices: ["==", "!=", "<=", "==="],
-        answer: "===",
-    }
-]
-var qIndex = 0;
-console.log(questions);
+  {
+    title: "Which is a Boolean value?",
+    choices: ["True", "Nope", "Yeah", "Nah"],
+    answer: "True",
+  },
+  {
+    title: "Which is NOT a semantic element?",
+    choices: ["Form", "nav", "aside", "div"],
+    answer: "div",
+  },
+  {
+    title: "Which is a self closeing tag",
+    choices: ["<aside>", "<figure>", "<article>", "<br>"],
+    answer: "<br>",
+  },
+  {
+    title: "Which comparison operator means strictly equal to?",
+    choices: ["==", "!=", "<=", "==="],
+    answer: "===",
+  },
+];
 
-var time = questions.length * 15;
-console.log(time);
+//refactoring script utilizing event listeners. Less functions to call on
 
-var timerId;
-// This function is going to identfy the time, and we count down
-function timeTrack() {
-        time--;
-        timeEl.textContent = time;
-        if(time <= 0) {
-            quizEnd();
-        }
+startEl.addEventListener("click", () => {
+  setTime();
+  titleEl.textContent = questions[0].title;
+  optionEl.forEach((a, b) => {
+    a.textContent = questions[0].choices[b];
+  });
+});
+
+function setTime() {
+  timeRemaining = 20;
+  timeEl.textContent = `Time: ${timeRemaining}`;
+  
 }
-// this function will display title, display questions Array.title
-
-function getQuestions() {
-    var currentQ = questions[qIndex];
-    titleEl.textContent = currentQ.title;
-    var currentQChoice = currentQ.choices;
-    choicesEl.textContent = currentQChoice;
-
-    for(var i = 0; i < currentQ.length; i++) {
-        var choiceBtn = $('<button>');
-        choiceBtn.addClass('choice-button btn btn-info');
-        choiceBtn.attr('data-choice', currentQ[i]);
-        choiceBtn.text(currentQ[i]);
-
-    }
-    
-    // for loop through the choices, each index process will create a Button
-    // set attribute to button class = choice, add vaule of index to the button 
-    // var . onlick = function, 
-}
-
-// after event listner we hide the welcome, unhide the view questions, and start the timeEl.
-function startQuiz () {
-        welcomeEl.setAttribute("class","view");
-        questionsEl.removeAttribute("class");
-        timerId = setInterval(timeTrack, 1000);
-        timeEl.textContent = time;
-        getQuestions();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-}
-
-startEl.addEventListener("click", startQuiz);
-
-
-function quizEnd(){
-    questionsEl.setAttribute("class", "view");
-    clearInterval(timerId);
-
-}
-
-//createElement("<div>") Element.Id("farely + i") 
